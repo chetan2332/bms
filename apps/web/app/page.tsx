c79ff35cc43f8e61777ee60b2452f28c41711874
Your Name <your.email@example.com>
2025-05-28 16:44:28 +0530

    Added prisma, added other dependencies

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -13,7 +13,7 @@ const ThemeImage = (props: Props) => {
 
   return (
     <>
-      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcLight} className="i mgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
   );
